7ab2b782ab2fa02893fa0330410d8348733a28a8
Kohei Morita <yosupo06@gmail.com>
2021-11-22 07:32:12 +0900

    WIP: add frac

---
+++ test/unittest/frac_test.cpp
@@ -0,0 +1,10 @@
+#include "yosupo/frac.hpp"
+#include "yosupo/random.hpp"
+
+#include <numeric>
+
+#include "gtest/gtest.h"
+
+using namespace yosupo;
+using ll = long long;
+using ull = unsigned long long;
